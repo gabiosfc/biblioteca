4569afea09c865f71c680aaad4e9573266fa0d01
Lucas Cunha <lucas.cunha.iftm@gmail.com>
2024-07-29 10:21:02 -0300

    FIrst Commit: Breeze e Spatie instalados. Seeder de user feito.

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    private $permissions_product = [
+        'product-list',
+        'product-create',
+        'product-edit',
+        'product-delete',
+    ];
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        $this->call([UserSeeder::class]);
+
+        foreach ($this->permissions_product as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        $adminRole = Role::create(['name' => 'Administrator']);
+        $managerRole = Role::create(['name' => 'Manager']);
+
+        foreach( Permission::all() as $permission){
+            $permission->assignRole($adminRole);
+        }
+ 
+        $adminUser = User::where('email', '=', 'usera@example.com')->first();
+        $adminUser->assignRole('Administrator');
+
+        $adminUser = User::where('email', '=', 'userb@example.com')->first();
+        $adminUser->assignRole('Manager');
+    }
+}
