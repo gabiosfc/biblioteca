85584aaaa24949fad556f5d22f11e3b039d181eb
Gabriela <gabiosfc@gmail.com>
2024-08-18 10:53:44 -0300

    Commit temporário antes do pull

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
 
     Route::resource('/products', ProductController::class)->middleware('role:Administrator');
+    //Route::get('/livro', ProductController::class)
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
